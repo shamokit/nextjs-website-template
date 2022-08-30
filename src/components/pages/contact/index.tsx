df020943018e756771666db2523c2b55c28d94f7
shamokit <contact@shamokit.com>
2022-08-30 17:21:08 +0900

    フォーム実装　仮

--- src/components/pages/contact/index.tsx
+++ src/components/pages/contact/index.tsx
@@ -1,8 +1,31 @@
-import { InnerLink } from '@/components/ui/link/InnerLink/index'
+import React from 'react'
+import { useForm } from 'react-hook-form'
+import axios from 'axios'
 export const PageContact = () => {
+	const {
+		register,
+		handleSubmit,
+		watch,
+		formState: { errors },
+	} = useForm()
+	const onSubmit = (data) => axios.post("/submit", data).then((res) => {
+		console.log(res)
+	}).catch((err) => {
+		console.log(err)
+	})
 	return (
-		<InnerLink href={'/'}>
-			<p>InnerLink</p>
-		</InnerLink>
+		<form onSubmit={handleSubmit(onSubmit)}>
+			<p>
+				<label htmlFor="name">Name</label>
+				<input id="name" {...register('name', { required: true })} />
+			</p>
+			{errors.name && <span>This field is required</span>}
+			<p>
+				<label htmlFor="email">Email</label>
+				<input id="email" {...register('email', { required: true })} />
+			</p>
+			{errors.email && <span>This field is required</span>}
+			<button type="submit">Submit</button>
+		</form>
 	)
 }
