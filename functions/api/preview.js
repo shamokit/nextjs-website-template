d4211cd7a56c46eadb30d65a84b84a63e3d626e8
shamokit <contact@shamokit.com>
2022-08-31 22:59:29 +0900

    テスト

---
+++ functions/api/preview.js
@@ -0,0 +1,58 @@
+export async function onRequestGet({ env, params }) {
+	try {
+		let secret = params.secret
+		let slug = params.slug
+
+		if (secret !== env.PREVIEW_SECRET_KEY || !slug) {
+			new Response('error', { status: 400 })
+		}
+		const fetchPreviewPage = async (pageSlug) => {
+			const data = await fetch(
+				`https://${env.NEWT_SPACE_U_KU ? env.NEWT_SPACE_U_KU : ''
+				}.api.newt.so/v1/staticPage/pageData`,
+				{
+					method: 'GET',
+					mode: 'cors',
+					headers: {
+						Authorization: `Bearer ${env.NEXT_PREVIEW_TOKEN ? env.NEXT_PREVIEW_TOKEN : ''
+							}`,
+					},
+					body: JSON.stringify({
+						query: {
+							limit: 1,
+							slug: pageSlug,
+							depth: 2,
+						},
+					}),
+				}
+			)
+
+			return { data: data.json() }
+		}
+		const { data } = await fetchPreviewPage(slug)
+		const pageData = data.items[0]
+		console.log(pageData)
+		// slugが存在しない場合、プレビューモードを有効にしないようにしましょう。
+		if (!pageData) {
+			return new Response('error', { status: 400 })
+		}
+
+		// Cookiesを設定し、プレビューモードを有効にします。
+		// Response..setPreviewData({})
+
+		const slugs = data.items.map((page) => page.slug)
+		const reverseSlugs = [...slugs].reverse()
+
+		const stringPaths = []
+		reverseSlugs.forEach((path, index) => {
+			const prev = stringPaths[index - 1]
+			stringPaths[index] = prev ? `${stringPaths[index - 1]}/${path}` : path
+		})
+		return Response.redirect(
+			`https://nextjs-website-template.pages.dev/${stringPaths[-1]}`,
+			301
+		)
+	} catch (err) {
+		return new Response(err, { status: 400 })
+	}
+}
