9e4c8a30051aea92582257a572bff8d1ddfcd0cc
shamokit <contact@shamokit.com>
2022-09-01 10:35:47 +0900

    テスト

--- functions/api/preview.js
+++ functions/api/preview.js
@@ -28,9 +28,9 @@ export async function onRequestGet({ env, request }) {
 				}
 			)
 
-			return { data: data.json() }
+			return { data: data }
 		}
-		const { data = JSON.parse(data) } = await fetchPreviewPage(slug)
+		const { data } = await fetchPreviewPage(slug)
 		return new Response(JSON.stringify(data), { status: 400 })
 		const pageData = data.items[0]
 
