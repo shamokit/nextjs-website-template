31de6588db5b1e5e87fa8411c035a7a4eb86d8f1
shamokit <contact@shamokit.com>
2022-12-21 02:35:16 +0900

    test

--- functions/api/preview.ts
+++ functions/api/preview.ts
@@ -1,20 +1,20 @@
 export const onRequestGet = async ({ params, env }) => {
-	const previewFetchConfig = {
-		headers: {
-			Authorization: `Bearer ${
-				env.NEXT_PREVIEW_TOKEN ? env.NEXT_PREVIEW_TOKEN : ''
-			}`,
-		}
-	}
-	const previewFetchUrl = `https://${
-		env.NEWT_SPACE_U_KU ? env.NEWT_SPACE_U_KU : ''
-	}.api.newt.so/v1`
-	const { query: {contentId, appUID, modelUID} } = params
-	if (!contentId) return
-	const res = await fetch(
-		`${previewFetchUrl}/${appUID}/${modelUID}/${contentId.toString()}`,
-		previewFetchConfig
-	)
-	const data = await res.json()
-  return new Response(JSON.stringify({ value: `${previewFetchUrl}/${appUID}/${modelUID}/${contentId.toString()}` }));
+	// const previewFetchConfig = {
+	// 	headers: {
+	// 		Authorization: `Bearer ${
+	// 			env.NEXT_PREVIEW_TOKEN ? env.NEXT_PREVIEW_TOKEN : ''
+	// 		}`,
+	// 	}
+	// }
+	// const previewFetchUrl = `https://${
+	// 	env.NEWT_SPACE_U_KU ? env.NEWT_SPACE_U_KU : ''
+	// }.api.newt.so/v1`
+	// const { query: {contentId, appUID, modelUID} } = params
+	// if (!contentId) return
+	// const res = await fetch(
+	// 	`${previewFetchUrl}/${appUID}/${modelUID}/${contentId.toString()}`,
+	// 	previewFetchConfig
+	// )
+	// const data = await res.json()
+  return new Response(JSON.stringify({ value: params }));
 };
