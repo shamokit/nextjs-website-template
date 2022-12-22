d1eefcd733e40f2f70264e12304d696f50a4a49a
shamokit <contact@shamokit.com>
2022-12-23 01:39:02 +0900

    test

--- functions/api/preview.ts
+++ functions/api/preview.ts
@@ -1,27 +1,72 @@
+type CmsName = 'newt' | 'microCMS'
 export const onRequestGet = async ({ request, env }) => {
 	const ErrorResponse = new Response('401 Unauthorized', { status: 401 })
-	const previewFetchConfig = {
-		headers: {
-			Authorization: `Bearer ${env.PREVIEW_TOKEN ? env.PREVIEW_TOKEN : ''}`,
-		},
-	}
-	const previewFetchUrl = `https://${
-		env.NEWT_SPACE_U_KU ? env.NEWT_SPACE_U_KU : ''
-	}.api.newt.so/v1`
-	const SECRET = env.SECRET
+	// CMS名を選んでください。
+	const cmsName: CmsName = 'newt'
+
+	// REQUEST URL
 	const url = new URL(request.url)
+
+	// check param exists
 	const params = new URLSearchParams(url.search)
 	if (!params) return ErrorResponse
+
+	// シークレットキーの確認
+	const SECRET = env.SECRET
 	const paramSECRET = params.get('secret')
 	if (SECRET !== paramSECRET) return ErrorResponse
-	const appUID = params.get('appUID')
-	const modelUID = params.get('modelUID')
-	const contentId = params.get('contentId')
-	if (!(appUID && modelUID && contentId)) return ErrorResponse
-	const res = await fetch(
-		`${previewFetchUrl}/${appUID}/${modelUID}/${contentId.toString()}`,
-		previewFetchConfig
-	)
+
+	// pathを生成 NewtとmicroCMSを想定しています。
+	const getCmsApiPathname = (cmsName: CmsName) => {
+		let path = ''
+		switch (cmsName) {
+			case 'newt': {
+				const appUID = params.get('appUID')
+				const modelUID = params.get('modelUID')
+				const contentId = params.get('contentId')
+				if (!(appUID && modelUID && contentId)) return ''
+				path = `${appUID}/${modelUID}/${contentId}`
+				break;
+			}
+			case 'microCMS': {
+				const endpoint = params.get('endpoint')
+				const contentId = params.get('contentId')
+				if (!(contentId)) return ''
+				path = `${endpoint}/${contentId}`
+				break;
+			}
+			default:
+				break;
+		}
+		return path
+	}
+	// headerを生成 NewtとmicroCMSを想定しています。
+	const getHeaders = (cmsName: CmsName) => {
+		let headers = {}
+		switch (cmsName) {
+			case 'newt': {
+				headers = {
+					Authorization: `Bearer ${env.CMS_API_KEY ? env.CMS_API_KEY : ''}`
+				}
+				break;
+			}
+			case 'microCMS': {
+				headers = {
+					"X-MICROCMS-API-KEY": `${env.CMS_API_KEY ? env.CMS_API_KEY : ''}`
+				}
+				break;
+			}
+			default:
+				break;
+		}
+		return headers
+	}
+
+	const baseUrl = env.CMS_API_URL
+	const cmsApiPathname = getCmsApiPathname(cmsName)
+	const headers = getHeaders(cmsName)
+	const fetchUrl = `${baseUrl}/${cmsApiPathname}`
+	const res = await fetch(fetchUrl, { headers })
 	const data = await res.json()
 	return new Response(JSON.stringify(data, null, 2), {
 		headers: {
