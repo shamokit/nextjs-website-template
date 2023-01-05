5826b6cba262e89e846cca56e5227ea731ebb500
shamokit <contact@shamokit.com>
2023-01-06 02:34:29 +0900

    デフォルトをmicrocmsに

--- functions/api/preview.ts
+++ functions/api/preview.ts
@@ -1,8 +1,8 @@
-type CmsName = 'newt' | 'microCMS'
+type CmsName = 'newt' | 'microcms'
 export const onRequestGet = async ({ request, env }) => {
 	const ErrorResponse = new Response('401 Unauthorized', { status: 401 })
 	// CMS名を選んでください。
-	const cmsName: CmsName = 'newt'
+	const cmsName: CmsName = 'microcms'
 
 	// REQUEST URL
 	const url = new URL(request.url)
@@ -15,8 +15,7 @@ export const onRequestGet = async ({ request, env }) => {
 	const SECRET = env.SECRET
 	const paramSECRET = params.get('secret')
 	if (SECRET !== paramSECRET) return ErrorResponse
-
-	// pathを生成 NewtとmicroCMSを想定しています。
+	// pathを生成
 	const getCmsApiPathname = (cmsName: CmsName) => {
 		let path = ''
 		switch (cmsName) {
@@ -28,7 +27,7 @@ export const onRequestGet = async ({ request, env }) => {
 				path = `${appUID}/${modelUID}/${contentId}`
 				break;
 			}
-			case 'microCMS': {
+			case 'microcms': {
 				const endpoint = params.get('endpoint')
 				const contentId = params.get('contentId')
 				const draftKey = params.get('draftKey')
@@ -41,7 +40,7 @@ export const onRequestGet = async ({ request, env }) => {
 		}
 		return path
 	}
-	// headerを生成 NewtとmicroCMSを想定しています。
+	// headerを生成
 	const getHeaders = (cmsName: CmsName) => {
 		let headers = {}
 		switch (cmsName) {
@@ -51,7 +50,7 @@ export const onRequestGet = async ({ request, env }) => {
 				}
 				break;
 			}
-			case 'microCMS': {
+			case 'microcms': {
 				headers = {
 					"X-MICROCMS-API-KEY": `${env.CMS_PREVIEW_API_KEY ? env.CMS_PREVIEW_API_KEY : ''}`
 				}
@@ -62,7 +61,6 @@ export const onRequestGet = async ({ request, env }) => {
 		}
 		return headers
 	}
-
 	const baseUrl = env.CMS_API_URL
 	const cmsApiPathname = getCmsApiPathname(cmsName)
 	const headers = getHeaders(cmsName)
