f3e91a21a0f4fceb30123e47e56cf56d9400e5f3
shamokit <contact@shamokit.com>
2023-01-06 02:36:58 +0900

    プレビュー周りをフックに移動

--- src/pages/preview/index.tsx
+++ src/pages/preview/index.tsx
@@ -1,8 +1,8 @@
 import type { NextPage } from 'next'
 import { useState } from 'react'
-import { useRouter } from 'next/router'
 import { NextSeo } from '@/libs/next-seo'
-import { previewApiClient } from '@/libs/newt-api-client'
+import { usePreview } from '@/libs/microcms-api-client'
+// import { usePreview } from '@/libs/newt-api-client'
 import { SITE_URL } from '@/utils/meta'
 import type { PageContent } from '@/schemas/staticPage/type'
 import { Breadcrumb } from '@/components/layout/breadcrumb/Breadcrumb'
@@ -16,45 +16,28 @@ type PageProps = {
 	status?: string
 }
 const StaticPage: NextPage<PageProps> = () => {
-	const [pageResponse, setPageResponse] = useState<PageContent>()
+	const [prevPageResponse] = usePreview<PageContent>()
 	const [breadcrumb, setBreadcrumb] = useState<BreadcrumbItemProps[]>()
-	const router = useRouter()
 	useEffect(() => {
-		const getPageData = async () => {
-			if (!router.isReady) return
-			const {
-				query: { contentId, appUID, modelUID, secret },
-			} = router
-			if (!appUID || !modelUID || !contentId || !secret) return
-			const pageResponse = await previewApiClient.preview.$get({
-				query: {
-					appUID: appUID.toString(),
-					modelUID: modelUID.toString(),
-					contentId: contentId.toString(),
-					secret: secret.toString(),
-				}})
-			if (!pageResponse) return
-			const { pageObjects: breadcrumbList } = generateBreadcrumbObjects(pageResponse)
-			setPageResponse(pageResponse)
-			setBreadcrumb(breadcrumbList)
-		}
-		getPageData()
-	}, [router])
+		if(!prevPageResponse) return
+		const { pageObjects: breadcrumbList } = generateBreadcrumbObjects(prevPageResponse)
+		setBreadcrumb(breadcrumbList)
+	}, [prevPageResponse])
 	const pageSlug = breadcrumb?.slice(-1)[0].path
 	const canonical = new URL(pageSlug ?? '', SITE_URL).toString()
 	return (
 		<>
-			{pageResponse && (
+			{prevPageResponse && (
 				<>
 					<NextSeo
-						titleTemplate={pageResponse.meta?.title}
-						title={pageResponse.meta?.title ?? pageResponse.title}
-						description={pageResponse.meta?.description ?? pageResponse.title}
+						titleTemplate={prevPageResponse.meta?.title}
+						title={prevPageResponse.meta?.title ?? prevPageResponse.title}
+						description={prevPageResponse.meta?.description ?? prevPageResponse.title}
 						canonical={canonical}
 						noindex
 					/>
 					<main>
-						<h1>{pageResponse.title}</h1>
+						<h1>{prevPageResponse.title}</h1>
 					</main>
 					{breadcrumb && <Breadcrumb list={breadcrumb} />}
 				</>
