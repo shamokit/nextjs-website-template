f5b36f658bb83d863f93340e8942862d74393950
shamokit <contact@shamokit.com>
2023-01-06 02:35:45 +0900

    プレビュー記事データの取得フックを追加

--- src/libs/microcms-api-client.ts
+++ src/libs/microcms-api-client.ts
@@ -1,3 +1,5 @@
+import { useState, useEffect } from 'react'
+import { useRouter } from 'next/router'
 import axios from 'axios'
 import aspida from '@aspida/axios'
 import api from '@/api/microcms/$api'
@@ -12,3 +14,31 @@ const fetchConfig = {
 	baseURL: process.env.CMS_API_URL
 }
 export const apiClient = api(aspida(axios, fetchConfig))
+const previewFetchConfig = {
+	baseURL: '/api'
+}
+export const previewApiClient = api(aspida(axios, previewFetchConfig))
+export const usePreview = <T>() => {
+	const [pageResponse, setPageResponse] = useState<T>()
+	const router = useRouter()
+	useEffect(() => {
+		const getPageData = async () => {
+			if (!router.isReady) return
+			const {
+				query: { endpoint, contentId, draftKey, secret },
+			} = router
+			if (!endpoint || !contentId || !draftKey || !secret) return
+			const pageResponse = await previewApiClient.preview.$get({
+				query: {
+					endpoint: endpoint.toString(),
+					contentId: contentId.toString(),
+					draftKey: draftKey.toString(),
+					secret: secret.toString(),
+				}}) as T
+			if (!pageResponse) return
+			setPageResponse(pageResponse)
+		}
+		getPageData()
+	}, [router])
+	return [pageResponse]
+}
--- src/libs/newt-api-client.ts
+++ src/libs/newt-api-client.ts
@@ -1,13 +1,15 @@
+import { useState, useEffect } from 'react'
+import { useRouter } from 'next/router'
 import axios from 'axios'
 import aspida from '@aspida/axios'
-import api from '@/api/$api'
+import api from '@/api/newt/$api'
 import type { Content, Contents, GetContentParams, GetContentQuery, GetContentsQuery } from 'newt-client-js'
 export type { Content, Contents, GetContentParams, GetContentQuery, GetContentsQuery }
 
 // Fetch CDN
 const fetchConfig = {
 	headers: {
-		Authorization: `Bearer ${process.env.CMS_CDN_API_KEY || ''}`,
+		Authorization: `Bearer ${process.env.CMS_PREVIEW_API_KEY || ''}`,
 	},
 	baseURL: process.env.CMS_API_URL?.replace('api.newt', 'cdn.newt')
 }
@@ -16,3 +18,27 @@ const previewFetchConfig = {
 	baseURL: '/api'
 }
 export const previewApiClient = api(aspida(axios, previewFetchConfig))
+export const usePreview = <T>() => {
+	const [pageResponse, setPageResponse] = useState<T>()
+	const router = useRouter()
+	useEffect(() => {
+		const getPageData = async () => {
+			if (!router.isReady) return
+			const {
+				query: { appUID, contentId, modelUID, secret },
+			} = router
+			if (!appUID || !contentId || !modelUID || !secret) return
+			const pageResponse = await previewApiClient.preview.$get({
+				query: {
+					appUID: appUID.toString(),
+					modelUID: modelUID.toString(),
+					contentId: contentId.toString(),
+					secret: secret.toString(),
+				}}) as T
+			if (!pageResponse) return
+			setPageResponse(pageResponse)
+		}
+		getPageData()
+	}, [router])
+	return [pageResponse]
+}
