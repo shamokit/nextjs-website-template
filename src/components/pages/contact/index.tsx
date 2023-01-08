48b12a6a9fadd6a54270242e8d03b75bd9a4396e
shamokit <contact@shamokit.com>
2023-01-09 01:47:32 +0900

    ほったらかしになってたのでいい感じに作業

--- src/components/pages/contact/index.tsx
+++ src/components/pages/contact/index.tsx
@@ -1,42 +1,78 @@
-import React from 'react'
-import { useForm } from 'react-hook-form'
-import axios from 'axios'
+import React, { useState } from 'react'
+import { axios } from '@/libs/axios'
+import { useForm } from '@/libs/react-hook-form'
+import { ErrorMessages } from '@/components/ui/error/ErrorMessages'
 type FormDataProps = {
 	name: string
 	email: string
 }
 export const PageContact = () => {
+	const [errorResponse, setErrorResponse] = useState<{
+		errors: { [key in keyof FormDataProps]?: string[] }
+	}>()
 	const {
 		register,
 		handleSubmit,
 		formState: { errors },
 	} = useForm<FormDataProps>()
+	type Rules = {
+		[key in keyof FormDataProps]: Parameters<typeof register>[1]
+	}
+	const rules: Rules = {
+		name: {
+			required: '名前を入力してください。',
+		},
+		email: {
+			required: 'メールアドレスを入力してください。',
+			pattern: {
+				value: /\S+@\S+\.\S+/,
+				message: 'メールアドレスの形式で入力してください。',
+			},
+		},
+	}
+	const formRequest = ({ url, data }: { url: string; data: FormDataProps }) => {
+		const params = new URLSearchParams()
+		Object.keys(data).forEach((name) => params.append(name, name))
+		axios
+			.post(url, data, {
+				headers: { 'content-type': 'application/json' },
+			})
+			.then((res) => {
+				console.log(res)
+			})
+			.catch((err) => {
+				setErrorResponse(err.response)
+			})
+	}
 	const onSubmit = (data: FormDataProps) => {
-		const formData = new FormData()
-		for (const [key, value] of Object.entries(data)) {
-			formData.append(key, value)
-		}
-		// axios
-		// 	.post('/api/contact', formData)
-		// 	.then((res) => {
-		// 		console.log(res)
-		// 	})
-		// 	.catch((err) => {
-		// 		console.log(err)
-		// 	})
+		formRequest({ url: '/api/contact', data })
 	}
 	return (
 		<form onSubmit={handleSubmit(onSubmit)}>
-			<p className='grid gap-2'>
-				<label htmlFor="name">名前</label>
-				<input id="name" {...register('name', { required: true })} />
-			</p>
-			{errors.name && <span>This field is required</span>}
-			<p className='grid gap-2'>
-				<label htmlFor="email">メールアドレス</label>
-				<input id="email" {...register('email', { required: true })} />
-			</p>
-			{errors.email && <span>This field is required</span>}
+			<div className="grid gap-2">
+				<label htmlFor="name">
+					名前<span>必須</span>
+				</label>
+				<ErrorMessages errors={errors.name?.message} />
+				<ErrorMessages errors={errorResponse?.errors.name} />
+				<input
+					id="name"
+					aria-invalid={errors.name ? 'true' : 'false'}
+					{...register('name', rules.name)}
+				/>
+			</div>
+			<div className="grid gap-2">
+				<label htmlFor="email">
+					メールアドレス<span>必須</span>
+				</label>
+				<ErrorMessages errors={errors.email?.message} />
+				<ErrorMessages errors={errorResponse?.errors.email} />
+				<input
+					id="email"
+					aria-invalid={errors.email ? 'true' : 'false'}
+					{...register('email', rules.email)}
+				/>
+			</div>
 			<button type="submit">送信する</button>
 		</form>
 	)
