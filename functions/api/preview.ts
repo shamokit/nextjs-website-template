1a7edb30cc1cce8a082e7774c1d7d93bea74c7f3
shamokit <contact@shamokit.com>
2023-01-09 01:46:04 +0900

    無駄な記述の削除

--- functions/api/preview.ts
+++ functions/api/preview.ts
@@ -1,6 +1,8 @@
-type CmsName = 'newt' | 'microcms'
+const cmsNames = ['newt', 'microcms'] as const
+type CmsName = typeof cmsNames[number]
 export const onRequestGet = async ({ request, env }) => {
 	const ErrorResponse = new Response('401 Unauthorized', { status: 401 })
+
 	// CMS名を選んでください。
 	const cmsName: CmsName = 'microcms'
 
@@ -15,56 +17,60 @@ export const onRequestGet = async ({ request, env }) => {
 	const SECRET = env.SECRET
 	const paramSECRET = params.get('secret')
 	if (SECRET !== paramSECRET) return ErrorResponse
+
 	// pathを生成
 	const getCmsApiPathname = (cmsName: CmsName) => {
-		let path = ''
+		if (!cmsNames.includes(cmsName)) return ''
 		switch (cmsName) {
 			case 'newt': {
 				const appUID = params.get('appUID')
 				const modelUID = params.get('modelUID')
 				const contentId = params.get('contentId')
 				if (!(appUID && modelUID && contentId)) return ''
-				path = `${appUID}/${modelUID}/${contentId}`
-				break;
+				return `${appUID}/${modelUID}/${contentId}`
 			}
 			case 'microcms': {
 				const endpoint = params.get('endpoint')
 				const contentId = params.get('contentId')
 				const draftKey = params.get('draftKey')
-				if (!(contentId)) return ''
-				path = `${endpoint}/${contentId}?draftKey=${draftKey}`
-				break;
+				if (!contentId) return ''
+				return `${endpoint}/${contentId}?draftKey=${draftKey}`
 			}
-			default:
-				break;
 		}
-		return path
 	}
 	// headerを生成
-	const getHeaders = (cmsName: CmsName) => {
-		let headers = {}
+	type GetHeader =
+		| {
+				Authorization: string
+		  }
+		| {
+				'X-MICROCMS-API-KEY': string
+		  }
+		| undefined
+	const getHeaders = (cmsName: CmsName): GetHeader => {
+		if (!cmsNames.includes(cmsName)) return undefined
 		switch (cmsName) {
 			case 'newt': {
-				headers = {
-					Authorization: `Bearer ${env.CMS_PREVIEW_API_KEY ? env.CMS_PREVIEW_API_KEY : ''}`
+				return {
+					Authorization: `Bearer ${
+						env.CMS_PREVIEW_API_KEY ? env.CMS_PREVIEW_API_KEY : ''
+					}`,
 				}
-				break;
 			}
 			case 'microcms': {
-				headers = {
-					"X-MICROCMS-API-KEY": `${env.CMS_PREVIEW_API_KEY ? env.CMS_PREVIEW_API_KEY : ''}`
+				return {
+					'X-MICROCMS-API-KEY': `${
+						env.CMS_PREVIEW_API_KEY ? env.CMS_PREVIEW_API_KEY : ''
+					}`,
 				}
-				break;
 			}
-			default:
-				break;
 		}
-		return headers
 	}
 	const baseUrl = env.CMS_API_URL
 	const cmsApiPathname = getCmsApiPathname(cmsName)
 	const headers = getHeaders(cmsName)
 	const fetchUrl = `${baseUrl}/${cmsApiPathname}`
+	if (!headers) return ErrorResponse
 	const res = await fetch(fetchUrl, { headers })
 	const data = await res.json()
 	return new Response(JSON.stringify(data, null, 2), {
