76255765d9b4888a658758d5fc6ec6b404eb55c8
shamokit <contact@shamokit.com>
2023-03-01 00:22:22 +0900

    fix: プロパティ名が違ったので修正

--- src/components/ui/accordion/Accordion/index.tsx
+++ src/components/ui/accordion/Accordion/index.tsx
@@ -5,11 +5,12 @@ export const Accordion: React.FC<AccordionProps> = ({
 	title,
 	children,
 	className,
-	...toggleProps
+	animation,
+	openDefault,
 }) => {
 	const accordionRef = useRef<HTMLDetailsElement>(null)
 	const accordionContentRef = useRef<HTMLDivElement>(null)
-	const [, doAccordion] = useToggle({ accordionRef, accordionContentRef, ...toggleProps })
+	const [, doAccordion] = useToggle({ accordionRef, accordionContentRef, animation, initialValue: openDefault })
 	return (
 		<details ref={accordionRef}>
 			<summary onClick={(e) => doAccordion(e)} className={className}>
