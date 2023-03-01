d94e578d3ccc8028c3d395ee9a9f5b80f9936440
shamokit <contact@shamokit.com>
2023-03-02 00:22:58 +0900

    fix: postcssの不要なプラグインの削除
    tailwindのクラスに置き換え

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,8 @@
 module.exports = {
 	"plugins": {
-		"postcss-import": {},
-		"tailwindcss/nesting": {},
 		"tailwindcss": {},
 		"autoprefixer": {},
 		"postcss-flexbugs-fixes": {},
-		"postcss-easings": {},
-		"postcss-at-rules-variables": {},
-		"postcss-custom-properties": {},
-		"postcss-simple-vars": {},
-		"postcss-mixins": {},
-		"postcss-nested": {},
 		"postcss-preset-env": {
 			"autoprefixer": {
 				"flexbox": "no-2009"
